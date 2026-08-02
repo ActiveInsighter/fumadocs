7c1a5728577f5e86cc263420614b71106b07deb5
Li Bowei <149855691+ActiveInsighter@users.noreply.github.com>
2026-08-02 13:23:04 +0800

    feat: prerender static search index

--- app/routes/search.ts
+++ app/routes/search.ts
@@ -1,10 +1,8 @@
-import type { Route } from './+types/search';
 import { createFromSource } from 'fumadocs-core/search/server';
 import { source } from '@/lib/source';
 
 const server = createFromSource(source);
 
-export async function loader({ request }: Route.LoaderArgs) {
-  return server.GET(request);
+export async function loader() {
+  return server.staticGET();
 }
-
